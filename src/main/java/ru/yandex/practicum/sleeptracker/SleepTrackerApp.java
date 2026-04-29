2d08f6b1a13b6b54a4f1c6d210dd1a734e8d1777
Kristina-Shab <krmatveeva@yandex.ru>
2026-04-29 19:40:07 +0300

    Реализовать чтение файла с логом

--- src/main/java/ru/yandex/practicum/sleeptracker/SleepTrackerApp.java
+++ src/main/java/ru/yandex/practicum/sleeptracker/SleepTrackerApp.java
@@ -1,8 +1,21 @@
 package ru.yandex.practicum.sleeptracker;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.function.Function;
+
 public class SleepTrackerApp {
 
     public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Для работы программы нужно указать путь к файлу с логом сна");
+            return;
+        }
 
+        String filePath = args[0];
+        List<SleepingSession> sessions = SessionFileLoader.getSessions(filePath);
+        System.out.println(sessions);
     }
 }
